8e368310fed4463debf53411caa53bb29a4918d4
altshifter <galemshizz@gmail.com>
2021-04-24 14:26:29 +0300

    task 6_2a, 6_2b

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,34 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip=input('ip adress: ')
+ip_oct=ip.split('.')
+big=False
+#проверка ввода
+if len(ip_oct) == 4 and (''.join(ip_oct)).isdigit():
+  i=-1
+  while i<len(ip_oct)-1:
+    i+=1
+    if ip_oct[i] and int(ip_oct[i]) < 256 and int(ip_oct[i]) >=0:
+      big=True
+    else:
+      big=False
+      break
+else:
+  print('Неправильный IP-адрес')  
+#последняя проверка и ответы
+if big:                                 
+  if ip == '0.0.0.0':
+    print('unassigned')
+  elif ip == '255.255.255.255':
+    print('local broadcast')
+  elif int(ip_oct[0]) <= 223:
+    print('unicast')
+  elif int(ip_oct[0]) <= 239 and int(ip_oct[0]) >= 224:
+    print('multicast')
+  else:
+    print('unused')
+else:
+  print('Неправильный IP-адрес')
+#это не глупо если это работает
--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -8,3 +8,38 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip=input('ip adress: ')
+ip_oct=ip.split('.')
+big=False
+ip_correct=False
+#проверка ввода
+
+while not ip_correct:
+  ip_oct=ip.split('.')
+  if len(ip_oct) == 4 and (''.join(ip_oct)).isdigit(): #4 октета и цифры
+    i=-1
+    while i<len(ip_oct)-1:
+      i+=1
+      if ip_oct[i] and int(ip_oct[i]) < 256 and int(ip_oct[i]) >=0: #непустой октет и не минусовое значение
+        big=True
+        ip_correct=True
+      else:
+        big=False
+        break
+  #последняя проверка и ответы
+  if big:
+    if ip == '0.0.0.0':
+      print('unassigned')
+    elif ip == '255.255.255.255':
+      print('local broadcast')
+    elif int(ip_oct[0]) <= 223:
+      print('unicast')
+    elif int(ip_oct[0]) <= 239 and int(ip_oct[0]) >= 224:
+      print('multicast')
+    else:
+      print('unused')
+  else:
+    print('Неправильный IP-адрес')
+    ip=input('ip adress: ')
+#это не глупо если это работает 2
+#но под конец запутался уже
