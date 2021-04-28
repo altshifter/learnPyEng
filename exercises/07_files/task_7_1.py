1a8bdb4ba5ccb49b1ccf0d6322d3c2478e5fe3a2
altshifter <galemshizz@gmail.com>
2021-04-28 19:16:44 +0300

    task 7_1

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -14,3 +14,14 @@ Outbound Interface    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+with open('ospf.txt') as f:
+    for line in f:
+        line = line.replace(",", " ").replace("[", "").replace("]", "")
+        line = line.split()
+        n1,ip,metr,n2,gw,tim,intf = line
+        print('''
+Prefix             {0:<25}
+AD/Metric          {1:<25}
+Next-Hop           {2:<25}
+Last update        {3:<25}
+Outbound Interface {4:<25}'''.format(ip,metr,gw,tim,intf))
